303db152228f547ccb88e8ad0bd2afc53ee0703b
thazinthazin <31156409+thazinthazin@users.noreply.github.com>
2019-09-30 17:46:12 +0630

    Update endofmonth_query

--- eom_query.php
+++ eom_query.php
@@ -1,17 +1,22 @@
 <?php
-  $link = new mysqli("localhost", "root", "", "account_oak") or die("Connect failed: %s\n". $link -> error);
+  // $link = new mysqli("localhost", "root", "", "account_oak") or die("Connect failed: %s\n". $link -> error);
+  include('config.php');
+
+  $sql = "SELECT * FROM `eom_status`";
+  $result = mysqli_query($link, $sql);
+        	
   echo "Connected Successfully.<br>";
 
     // Check if form is submitted successfully 
     if(isset($_POST["submit"]))  
     { 
         // Check if any option is selected 
-        if(isset($_POST["stdid"]) && ($_POST["title"]) && ($_POST["payableamount"]) && ($_POST["createdon"]))
+        if(isset($_POST["payment_no"]) && ($_POST["monthly_amount"]) && ($_POST["eom_date"]))
         { 
-        	$name = $_POST["title"];
-        	$credit = $_POST["payableamount"];
-        	$debit = $_POST["payableamount"];
-        	$date = $_POST["createdon"];
+        	$name = $_POST["payment_no"];
+        	$credit = $_POST["monthly_amount"];
+        	$debit = $_POST["monthly_amount"];
+        	$date = $_POST["eom_date"];
         	$income_accountid = 4;  // constant
 			$defer_incomeid = 9;  // constant
 			$productid = 1;  // constant
@@ -19,15 +24,11 @@
 			$paymentid = 1;  // constant
 			$invoiceid = 1;  // constant
 			$journalentryid = 0;
-
 			$item_sql = "";
-
             // Retrieving each selected option 
-            for ($i=0; $i < sizeof($_POST["stdid"]); $i++) {
-
+            for ($i=0; $i < sizeof($_POST["name"]); $i++) {
             	$check_paymentno_sql = "SELECT * FROM `journal_entry` WHERE `name` = '$name[$i]' ORDER BY id DESC LIMIT 1";
             	$result = $link->query($check_paymentno_sql);
-
 				if ($result->num_rows > 0) {
 				    // output data of each row
 				    while($row = $result->fetch_assoc()) {
@@ -38,28 +39,23 @@
             	      			
 	      		// income amount
 				$item_sql .= "INSERT INTO `journal_item`(`name`,`productid`,`credit`,`accountid`,`journalentryid`,`journalid`,`paymentid`,`invoiceid`,`createdon`)
-				VALUES ('$name[$i]','$productid','$credit[$i]','$income_accountid','$journalentryid','$journalid','$paymentid','$invoiceid','$date[$i]');";
-
+				VALUES ('$name','$productid','$credit','$income_accountid','$journalentryid','$journalid','$paymentid','$invoiceid','$date');";
 				// defer income amount
 				$item_sql .= "INSERT INTO `journal_item`(`name`,`productid`,`debit`,`accountid`,`journalentryid`,`journalid`,`paymentid`,`invoiceid`,`createdon`)
-				VALUES ('$name[$i]','$productid','$debit[$i]','$defer_incomeid','$journalentryid','$journalid','$paymentid','$invoiceid','$date[$i]');";
-
+				VALUES ('$name','$productid','$debit','$defer_incomeid','$journalentryid','$journalid','$paymentid','$invoiceid','$date');";
 				$journalentryid = 0;
             }
             echo " <br> SQL : " . $item_sql . "<br>";
-
 			if (mysqli_multi_query($link, $item_sql)) {
 				echo json_encode(array("statusCode"=>200));
 			} 
 			else {
 				echo json_encode(array("statusCode"=>201));
 			}
-
 			mysqli_close($link);
         } 
     else
         echo "Select an option first !!"; 
     } 
     return 0;
-
 ?>
