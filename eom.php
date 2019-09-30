3a27bfbc7815e71a7ebe5b3dd2c671c70a32f66f
thazinthazin <31156409+thazinthazin@users.noreply.github.com>
2019-09-30 23:10:34 +0630

    Update eom php

--- eom.php
+++ eom.php
@@ -1,109 +1,120 @@
 <?php
-
-$link = new mysqli("localhost", "root", "", "account_oak") or die("Connect failed: %s\n". $link -> error);
-
+  	$link = new mysqli("localhost", "root", "", "account_oak") or die("Connect failed: %s\n". $link -> error);
+  	
+	$sql = "SELECT * FROM `eom_status`";
+  	$result = mysqli_query($link, $sql);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" rel="stylesheet"/>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
-<style type="text/css">
-h2 {
-  display: inline-block;
-  text-align: left;
-  font-weight: 700;
-  font-size: 1.875rem;
-  margin-bottom: .5rem;
-  font-family: inherit;
-  line-height: 1.2;
-  color: inherit;
-}
-th {
-    text-align: inherit;
-    color: #306136;
-}
-</style>
+	<title>EOM Status</title>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
 
-    <h2>End Of Month Status</h2>
-
-	<?php 
-	// Check connection
-	if($link === false){
-		 die("ERROR: Could not connect. " . mysqli_connect_error());
-	}
-
-	// Attempt select query execution
-	$sql = "SELECT pd.`id`, pd.`title`, pd.`payableamount`, pd.`createdon` FROM `payment_details`AS pd";
-
-	if($result = mysqli_query($link, $sql)){
-		if(mysqli_num_rows($result) > 0){ 
-	?>
-
-	<form action="eom_query.php" method="post">
-		<!-- <input type="hidden" name="rowid[]" value="true" /> -->
-
-		<table class="admintable generaltable">
-			<thead>
-				<tr>
-					<th><input type="checkbox" id="checkAll"/></th>
-	            	<th>#</th>
-	                <th>Name</th>
-	                <th>Amount</th>
-	                <th>Date</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				while($row = mysqli_fetch_array($result)){ ?>
-	                <tr>
-	                    <td><input type ="checkbox" class="checkbox" id="<?php echo $row['id'] ?>" name="rowid[]" /> </td>
-	                    <td><?php echo $row['id'] ?><input type="hidden" name="stdid[]" value="<?php echo $row['id'] ?>"></td>
-	                    <td><?php echo $row['title'] ?><input type="hidden" name="title[]" value="<?php echo $row['title'] ?>"></td>
-	                    <td><?php echo $row['payableamount'] ?><input type="hidden" name="payableamount[]" value="<?php echo $row['payableamount'] ?>"></td>
-	                    <td><?php echo $row['createdon'] ?><input type="hidden" name="createdon[]" value="<?php echo $row['createdon'] ?>"></td>
-	                </tr>
-	            <?php } ?>
-			</tbody>
-		</table>
-
-		<button type="submit" class="btn btn-primary" id="submit" name="submit">Run EOM</button>
-	</form>
+<form id="test-form" action="eom_query.php" method="post">
+  	<input type="hidden" name="formdata" id="formdata" value="">
+  <table class="table table-bordered">
+  	<!-- <input type="hidden" name="data"> -->
+    <thead>
+      <tr>
+    	<th>#</th>
+        <th>Payment No</th>
+        <th>Monthly Amount</th>
+        <th>Date</th>
+        <th><input type="checkbox" id="chkAll" onchange="checkAll(this)"/></th>
+      </tr>
+    </thead>
+    <tbody>
+    	<?php
+			$count=1;
+			while($row = mysqli_fetch_array($result)) { ?>
+	      <tr id="6" role="row" class="odd">
+	        <td data-content="ratio"><?php echo $count; ?></td>
+	        <td class="sorting_1" data-content="domain"><?php echo $row['payment_no'] ?></td>
+	        <td class="sorting_1" data-content="matched"><?php echo $row['monthly_amount'] ?></td>
+	        <td class="sorting_1" data-content="matched"><?php echo $row['eom_date'] ?></td>
+	        <td><input name="id[]" value="" type="checkbox"></td>
+	      </tr>
+	    <?php $count++; } ?>
+    </tbody>
+  </table>
+  <button class="btn btn-success" type="submit" value="save">Run EOM</button>
+</form>
 
 <?php 
-	// Free result set
-	mysqli_free_result($result);
-} else{
- 	echo "<em>No records were found.</em>";
-}} else{
-  	echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
- }
-
- // Close connection
- mysqli_close($link);
-
+mysqli_close($link);
 ?>
 
 <script type="text/javascript">
-	$(document).ready(function(){
-		$('#checkAll').click(function(){
-			if (this.checked) {
-				$('.checkbox').each(function(){
-					this.checked = true;
-				});
-			} else {
-				$('.checkbox').each(function(){
-					this.checked = false;
-				});
-			}
-		});
-	});
+	function checkAll(ele) {
+	     var checkboxes = document.getElementsByTagName('input');
+	     if (ele.checked) {
+	         for (var i = 0; i < checkboxes.length; i++) {
+	             if (checkboxes[i].type == 'checkbox') {
+	                 checkboxes[i].checked = true;
+	             }
+	         }
+	         alert("All checked");
+	     } else {
+	         for (var i = 0; i < checkboxes.length; i++) {
+	             console.log(i)
+	             if (checkboxes[i].type == 'checkbox') {
+	                 checkboxes[i].checked = false;
+	             }
+	         }
+	         alert("Unchecked");
+	     }
+	}
 
+	// First, I'm stopping the default behaviors
+	$("button").on("click", function(event){
+	  event.stopPropagation();
+	  event.preventDefault();
+	})
+
+	// Now, when we click save, I want to format
+	//  a string.
+	$("button[value='save']").on("click", function(){
+	  var returnString = "";
+	  
+	  // The following selector gets all the rows
+	  //   that have a CHECKED checkbox. Note I
+	  //   don't get the checkbox, simply the row.
+	  var rowEls = $("#test-form").find("tr:has(input[type='checkbox']:checked) ");
+	  
+	  // For every row, we'll add to our string...
+	  rowEls.each(function(){
+	    var rowEl = $(this);
+	    
+	    // First, a row id
+	    // returnString += rowEl.attr("id")+" - ";
+	    var cells = rowEl.children("td");
+	    
+	    // Next the contents of each cell THAT
+	    //  HAS A data-content ATTRIBUTE. This
+	    //  way, we don't get the remove/keep.
+	    cells.each(function(){
+	      if($(this).data("content")){
+	        // returnString += $(this).data("content")+"="+$(this).text();
+	        returnString += $(this).text();
+	         // if ($(this).is(':last-child')) {returnString += " | ";}
+	         if ($(this).is(':nth-last-child(2)')) {returnString += "";}
+	        else {returnString += " , ";}
+
+	      }
+	    })
+	    
+	    returnString += " | ";
+	  })
+	  
+	  console.log(returnString);
+	  $('#formdata').val(returnString);
+	  $("#test-form").submit();
+	    
+	})
 </script>
 
 </body>
