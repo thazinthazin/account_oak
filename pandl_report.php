860dbd6482eb13f434d96b0bef855c88e9bac266
thazinthazin <31156409+thazinthazin@users.noreply.github.com>
2019-10-23 00:38:33 +0630

    date filter in P&L report

--- pandl_report.php
+++ pandl_report.php
@@ -21,7 +21,10 @@ echo $OUTPUT->header();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.js"></script>
 <style type="text/css">
 h2 {
   display: inline-block;
@@ -33,39 +36,53 @@ h2 {
   line-height: 1.2;
   color: inherit;
 }
-th {
-    text-align: inherit;
-    color: #306136;
-}
 </style>
 </head>
 <body>
 
-    <div class="row">
-      <h2 class="pull-left">Profit & Loss Report</h2>
-    </div>
+<div class="row">
+  <h2 class="pull-left">Profit & Loss Report</h2>
+</div>
 
+  <?php
+    $startDate = (isset($_POST["from_date"])) ? $_POST["from_date"] : '';
+    $endDate = (isset($_POST["to_date"])) ? $_POST["to_date"] : '';
+  ?>
+
+<form action="p&l_report.php" method="post">
+  <div class="input-daterange">
     <div class="col-md-2">
       <div class="form-group">
         <label for="from_date">Start Date</label>
-        <input type="text" id="from_date" class="form-control" name="from_date">
+        <input type="text" id="from_date" class="form-control" name="from_date" value="<?php echo $startDate; ?>">
       </div>
     </div>
 
     <div class="col-md-2">
       <div class="form-group">
         <label for="to_date">End Date</label>
-        <input type="text" id="to_date" class="form-control" name="to_date">
+        <input type="text" id="to_date" class="form-control" name="to_date" value="<?php echo $endDate; ?>">
       </div>
     </div>
+  </div>
 
-    <button type="submit" id="filter" class="btn btn-primary" name="filter">Filter</button>
+  <button type="submit" id="filter" class="btn btn-primary" name="filter">Filter</button>
+</form>
     
     <div class="row">
       <h3 class="pull-left">Income</h3>
     </div>
 
     <div id="order_table">
+
+      <?php 
+        // totalIncome($link, '2019-08-01','2019-10-30');
+        // totalIncome($link, '','');
+        totalIncome($link, $startDate, $endDate);
+
+      function totalIncome($link, $startDate='', $endDate=''){
+      ?>
+
      <table class="table">
       <tbody>
         <?php
@@ -74,9 +91,16 @@ th {
           JOIN `account_journal` AS acj ON acj.`id` = jt.`journalid`
           JOIN `journal_item` AS ji ON ji.`journalentryid` = jt.`id`
           JOIN `chart_of_account` AS coa ON coa.`id` = ji.`accountid`
-          WHERE coa.`id` = 10 GROUP BY acj.`name`";
-        $result = mysqli_query($link, $sql);
+          WHERE coa.`id` = 10 ";
+          if ($startDate != '' && $endDate != '') {
+            $sql .= "AND FROM_UNIXTIME(ji.`createdon`,'%Y-%m-%d') BETWEEN '$startDate' AND '$endDate' ";
+          }
+          
+          $sql .= "GROUP BY acj.`name`";
 
+          // echo $sql . "<br/>";
+
+        $result = mysqli_query($link, $sql);
         while($row = mysqli_fetch_array($result)) { ?>
           <tr>
             <td><?php echo $row['journalname'] ?></td>
@@ -86,9 +110,19 @@ th {
       </tbody>
     </table>
 
+  <?php } ?>
+
     <div class="row">
       <h3 class="pull-left">Expenses</h3>
     </div>
+
+    <?php 
+     // totalExpense($link, '2019-08-01','2019-10-30');
+        // totalExpense($link, '','');
+    totalExpense($link, $startDate, $endDate);
+
+    function totalExpense($link, $startDate='', $endDate=''){
+    ?>
 
      <table class="table">
       <tbody>
@@ -98,9 +132,16 @@ th {
           JOIN `account_journal` AS acj ON acj.`id` = jt.`journalid`
           JOIN `journal_item` AS ji ON ji.`journalentryid` = jt.`id`
           JOIN `chart_of_account` AS coa ON coa.`id` = ji.`accountid`
-          WHERE coa.`id` = 3 GROUP BY acj.`name`";
-        $result = mysqli_query($link, $sql);
+          WHERE coa.`id` = 3 ";
+          if ($startDate != '' && $endDate != '') {
+            $sql .= "AND FROM_UNIXTIME(ji.`createdon`,'%Y-%m-%d') BETWEEN '$startDate' AND '$endDate' ";
+          }
+          
+          $sql .= "GROUP BY acj.`name`";
 
+          // echo $sql . "<br/>";
+
+        $result = mysqli_query($link, $sql);
         while($row = mysqli_fetch_array($result)) { ?>
           <tr>
             <td><?php echo $row['journalname'] ?></td>
@@ -109,45 +150,25 @@ th {
         <?php } ?>
       </tbody>
     </table>
+
+    <?php } ?>
+
   </div>
 
 <?php
 // Close connection
 mysqli_close($link);
-
 echo $OUTPUT->footer();
 ?>
 
 <script type="text/javascript">
   $(document).ready(function(){
-    $.datepicker.setDefaults({
-      dateFormat:'yy-mm-dd'
-    });
-    $(function(){
-      $("#from_date").datepicker();
-      $("#to_date").datepicker();
-    });
-    $('#filter').click(function(){
-      var from_date = $('#from_date').val();
-      var to_date = $('#to_date').val();
-      if (from_date != '' && to_date != '')
-      {
-        $.ajax({
-          url : "p&l_report.php",
-          method : "POST",
-          data : {from_date:from_date, to_date:to_date},
-          success:function(data)
-          {
-            $('#order_table').html(data);
-          }
-        });
-      }
-      else
-      {
-        alert("Please Select Date");
-      }
-    })
-  });
+ $('.input-daterange').datepicker({
+  todayBtn:'linked',
+  format: "yyyy-mm-dd",
+  autoclose: true
+ });
+});
 </script>
 
 </body>
