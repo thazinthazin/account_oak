3477a9227c111fcb6ae70611596b76f825581db9
thazinthazin <31156409+thazinthazin@users.noreply.github.com>
2019-10-23 17:47:45 +0630

    update report file

--- report.php
+++ report.php
@@ -15,13 +15,64 @@ $PAGE->navbar->add('Accounting Report', new moodle_url('report.php'));
 
 echo $OUTPUT->header();
 
-$sql = "SELECT jt.`name`, jt.`totalamount`, acj.`name` AS journalname, ji.`createdon`, ji.`debit`, ji.`credit`, coa.`name` AS coaname, mc.`fullname` 
+// $sql = "SELECT jt.`name`, jt.`totalamount`, acj.`name` AS journalname, FROM_UNIXTIME(ji.`createdon`,'%Y-%m-%d') AS createdon, ji.`debit`, ji.`credit`, coa.`name` AS coaname, mc.`fullname` 
+//         FROM `journal_entry` AS jt
+//         JOIN `account_journal` AS acj ON acj.`id` = jt.`journalid`
+//         JOIN `journal_item` AS ji ON ji.`journalentryid` = jt.`id`
+//         JOIN `chart_of_account` AS coa ON coa.`id` = ji.`accountid`
+//         JOIN `mdl_course` AS mc ON mc.`id` = ji.`productid`";
+// $result = mysqli_query($link, $sql);
+
+$ArrFinal=[];
+
+$sql = "SELECT ji.`productid`,jt.`name`, jt.`totalamount`, acj.`name` AS journalname, FROM_UNIXTIME(ji.`createdon`,'%Y-%m-%d') AS createdon, ji.`debit`, ji.`credit`, coa.`name` AS coaname
         FROM `journal_entry` AS jt
         JOIN `account_journal` AS acj ON acj.`id` = jt.`journalid`
         JOIN `journal_item` AS ji ON ji.`journalentryid` = jt.`id`
-        JOIN `chart_of_account` AS coa ON coa.`id` = ji.`accountid`
-        JOIN `mdl_course` AS mc ON mc.`id` = ji.`productid`";
-$result = mysqli_query($link, $sql);
+        JOIN `chart_of_account` AS coa ON coa.`id` = ji.`accountid`";
+
+          if($result_all = mysqli_query($link, $sql))
+          {
+            if(mysqli_num_rows($result_all) > 0)
+            {
+              while($row=mysqli_fetch_assoc($result_all))
+              {
+                 $jname=$row["journalname"];
+                 $tmpArr=[];
+                 $tmpArr["name"]=$row["name"];
+                 $tmpArr["totalamount"]=$row["totalamount"];
+                 $tmpArr["journalname"]=$row["journalname"];
+                 $tmpArr["createdon"]=$row["createdon"];
+                 $tmpArr["debit"]=$row["debit"];
+                 $tmpArr["credit"]=$row["credit"];
+                 $tmpArr["coaname"]=$row["coaname"];
+                 $sql_title = "";
+
+                 if($jname===$coursefee)
+                 {
+                  $sql_title = "SELECT mc.`fullname` AS title FROM `mdl_course` mc WHERE mc.`id`=".$row["productid"];
+                 }
+                 else if($jname===$payroll)
+                 {
+                  $sql_title = "SELECT CONCAT(u.`firstname`,' ', u.`lastname`,'(' ,p.`pay_month`,' ',p.`pay_year`,')') AS title
+                              FROM `mdl_basic_salary_payroll` p 
+                              join `mdl_user` u on p.`userid`=u.`id` WHERE p.`id`=".$row["productid"];
+                 }
+
+                  if($result_title= mysqli_query($link, $sql_title))
+                  {
+                    if(mysqli_num_rows($result_title) > 0)
+                    {
+                      while($rowT=mysqli_fetch_assoc($result_title))
+                      {
+                        $tmpArr["title"]=$rowT["title"];
+                      }
+                    }
+                  }
+                  $ArrFinal[]=$tmpArr;
+              }
+            }
+          }
 
 ?>
 
@@ -75,7 +126,7 @@ th {
             <td><?php echo $count; ?></td>
             <td><?php echo $row['name'] ?></td>
             <td><?php echo $row['journalname'] ?></td>
-            <td><?php echo $row['fullname'] ?></td>
+            <td><?php echo $row['title'] ?></td>
             <td><?php echo $row['totalamount'] ?></td>
             <td><?php echo $row['debit'] ?></td>
             <td><?php echo $row['credit'] ?></td>
